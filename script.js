cdc4b4975bb3a6797faab43981733ace7392b4d7
sauravets <saurav.dubey.ets@gmail.com>
2022-03-15 19:51:04 +0530

    jquery-start-designing

---
+++ script.js
@@ -0,0 +1,15 @@
+    $(document).ready(function () {
+    $("#myinput").keyup(function () {   
+        let txt = $("#myinput").val();           
+        $("#mydiv").html(txt);        
+    });
+
+    // $(".black").addClass("black");
+    // $("#first").css( "background,gray","height,100px","width,100px","border-radius, 50px",
+    // "border, 1px solid black", "display,none");       
+    $("<div>").attr({
+        'class':'black',
+        'style':"background:gray,height:100px,width:100px,border-radius:50px;"
+    }).append("#main"); 
+
+});
