dc6d4712e521810c2a0bf350d637a949dd965bc1
sauravets <saurav.dubey.ets@gmail.com>
2022-03-16 19:54:31 +0530

    static-design

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ $(document).ready(function () {
         $("#mydiv").html(txt);
     });
 
-    // $(".black").addClass("black");
-    // $("#first").css( "background,gray","height,100px","width,100px","border-radius, 50px",
-    // "border, 1px solid black", "display,none");       
-    $("<div>").attr({
-        'class': 'black',
-        'style': "background:gray,height:100px,width:100px,border-radius:50px;"
-    }).append("#main");
-
+    $(".column").click(function(){
+        $(".tick-mark").toggle();
+        
+    });
 });
