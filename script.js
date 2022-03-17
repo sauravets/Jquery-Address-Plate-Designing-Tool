7628c7b85e5ec15291c066cc631bb6c9b055169c
sauravets <saurav.dubey.ets@gmail.com>
2022-03-17 20:29:24 +0530

    add-style

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ $(document).ready(function () {
         $("#mydiv").html(txt);
     });
 
-    $(".column").click(function(){
+    $(".column").click(function () {
         $(".tick-mark").toggle();
-        
+        let new_color = $(this).data("bgcolor");
+        $("#mydiv").css({ backgroundColor: new_color });
+    });
+
+    $(".text-color").click(function () {
+        let txt_color = $(this).data("color");
+        $("#mydiv").css({ color: txt_color });        
+    });
+
+    $(".font-style").click(function () {
+        let font_style = $(this).data("font");
+        $("#mydiv").css({ fontFamily: font_style });
+        $("#mydiv").css({ 'fontSize': '45px' });
+        $("#mydiv").css({ 'text-transform': 'uppercase' });
     });
 });
