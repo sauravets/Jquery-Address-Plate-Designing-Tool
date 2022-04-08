cd7faf90e53240baa69c2187ab061bc789e67ce7
sauravets <saurav.dubey.ets@gmail.com>
2022-04-08 14:11:01 +0530

    done

--- script.js
+++ script.js
@@ -103,44 +103,45 @@ jQuery(document).ready(function () {
     }
     // jQuery("#search").on("keyup", function (e) {
     //     e.preventDefault();
-    // let value = jQuery(this).val().toLowerCase();              
-    // jQuery("#tbody tr").filter(function () {            
-    //     jQuery(this).toggle(jQuery(this).text().toLowerCase().indexOf(value) > -1)                           
+    //     let value = jQuery(this).val().toLowerCase();
+    //     jQuery("#tbody tr").filter(function () {
+    //         jQuery(this).toggle(jQuery(this).text().toLowerCase().indexOf(value) > -1)
+    //     });
+        jQuery("#search").on("keyup", function () {
+            let value = jQuery(this).val().toLowerCase();
+            jQuery("table tr").each(function (index) {
+                if (index != 0) {
+                    $row = jQuery(this);
+                    let id = $row.find("td:first").text().toLowerCase();
+                    if (id.indexOf(value) != 0) {
+                        jQuery(this).hide();
+                    }
+                    else {
+                        jQuery(this).show();
+                    }
+                }
+            });
+        });
     // });
-    // jQuery("#search").on("keyup", function () {
-    // let value = jQuery(this).val().toLowerCase();
-    // jQuery("table tr").each(function(index) {
-    //     if (index != 0) {
-    //         $row = jQuery(this);
-    //         let id = $row.find("td:first").text().toLowerCase();
-    //         if (id.indexOf(value) != 0) {
-    //             jQuery(this).hide();
-    //         }
-    //         else {
-    //             jQuery(this).show();
+
+    // jQuery("#search").keyup(function () {
+    //     let value = jQuery(this).val().toLowerCase();
+    //     let tbody = '';
+    //     let object = JSON.parse(localStorage.getItem('Users'));               
+    //     for(key in object){
+    //         for (let i = 0; i <= key.length-1; i++) {
+    //             let get_object = key[i];
+    //             console.log(get_object);
+    //             tbody += "<tr  id='tbody" + "'>";
+    //             tbody += "<td>" + get_object.FirstName + "</td>";
+    //             tbody += "<td>" + get_object.LastName + "</td>";
+    //             tbody += "<td>" + get_object.Email + "</td>";
+    //             tbody += "<td>" + get_object.Address_Plate + "</td>";
+    //             tbody += "<td>" + get_object.Background + "</td>";
+    //             tbody += "<td>" + get_object.TextColor + "</td>";
+    //             tbody += "<td>" + get_object.FontStyle + "</td>";
+    //             tbody += "</tr>";
     //         }
     //     }
-    // });
-    jQuery("#search").keyup(function () {
-        let value = jQuery(this).val().toLowerCase();
-        let tbody = '';
-        let object = JSON.parse(localStorage.getItem('Users'));
-        for (let i = 0; i <= object.length; i++) {
-            let get_object = object[i];
-            if (value) {
-                if (get_object.indexOf(value) > -1) {
-                    tbody += "<tr  id='tbody" + "'>";
-                    tbody += "<td>" + takeObject.FirstName + "</td>";
-                    tbody += "<td>" + takeObject.LastName + "</td>";
-                    tbody += "<td>" + takeObject.Email + "</td>";
-                    tbody += "<td>" + takeObject.Address_Plate + "</td>";
-                    tbody += "<td>" + takeObject.Background + "</td>";
-                    tbody += "<td>" + takeObject.TextColor + "</td>";
-                    tbody += "<td>" + takeObject.FontStyle + "</td>";
-                    tbody += "</tr>";
-                }
-            }
-        }
-    })
     // });
 });
